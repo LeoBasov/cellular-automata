1f600158cd2943c065ccf96d8592916f7c46e702
Leo Basov <you@example.com>
2022-07-21 12:20:03 +0200

    updated gui

--- src/gui/gui.cc
+++ src/gui/gui.cc
@@ -4,4 +4,27 @@ namespace ca {
 
 GUI::GUI() {}
 
+void GUI::displayMe(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINE_LOOP);
+    {  // GL_POLYGON
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.5, 0.5, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+    }
+    glEnd();
+    glFlush();
+}
+
+void GUI::Run(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
+}
+
 }  // namespace ca
--- src/gui/gui.h
+++ src/gui/gui.h
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <GL/glut.h>
+
 namespace ca {
 
 class GUI {
    public:
     GUI();
     ~GUI() = default;
+
+    static void displayMe(void);
+    void Run(int argc, char** argv);
 };
 
 }  // namespace ca
