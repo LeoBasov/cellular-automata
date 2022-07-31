fab5fcd8be7eb05a8f77497e9e3d8f836ef85921
Leo Basov <l.basov@sparc-industries.com>
2022-07-31 18:44:36 +0200

    added kernel name space

---
+++ src/al/kernel.cc
@@ -0,0 +1,20 @@
+#include "kernel.h"
+
+namespace ca {
+
+double GameOfLife(const Grid& grid, const int& x, const int& y) {
+    double count = 0;
+
+    count += grid.value(x - 1, y);
+    count += grid.value(x - 1, y - 1);
+    count += grid.value(x, y - 1);
+    count += grid.value(x + 1, y - 1);
+    count += grid.value(x + 1, y);
+    count += grid.value(x + 1, y + 1);
+    count += grid.value(x, y + 1);
+    count += grid.value(x - 1, y + 1);
+
+    return count;
+}
+
+}  // namespace ca
---
+++ src/al/kernel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../grid/grid.h"
+
+namespace ca {
+
+double GameOfLife(const Grid& grid, const int& x, const int& y);
+
+}  // namespace ca
