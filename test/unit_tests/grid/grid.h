cd52580b427886df0497d9cee48f75b8f32448cb
Leo Basov <l.basov@sparc-industries.com>
2022-07-31 17:58:39 +0200

    extended unit tests

--- test/unit_tests/grid/grid.h
+++ test/unit_tests/grid/grid.h
@@ -35,4 +35,32 @@ TEST(Grid, Constructor) {
     }
 }
 
+TEST(Grid, value) {
+    const uint x = 5;
+    const uint y = 6;
+    Grid grid(x, y);
+
+    grid.value(0, 0) = 1.0;
+    grid.value(4, 0) = 2.0;
+
+    grid.value(0, 5) = 3.0;
+    grid.value(4, 5) = 4.0;
+
+    ASSERT_EQ(1.0, grid.value(0, 0));
+    ASSERT_EQ(1.0, grid.value(5, 0));
+    ASSERT_EQ(1.0, grid.value(0, 6));
+
+    ASSERT_EQ(2.0, grid.value(4, 0));
+    ASSERT_EQ(2.0, grid.value(-1, 0));
+    ASSERT_EQ(2.0, grid.value(4, 6));
+
+    ASSERT_EQ(3.0, grid.value(0, 5));
+    ASSERT_EQ(3.0, grid.value(5, 5));
+    ASSERT_EQ(3.0, grid.value(0, -1));
+
+    ASSERT_EQ(4.0, grid.value(4, 5));
+    ASSERT_EQ(4.0, grid.value(-1, 5));
+    ASSERT_EQ(4.0, grid.value(4, -1));
+}
+
 }  // namespace ca
