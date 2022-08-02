4abdf7e861b3c92f1db1044edbe8c1bf6de3daa7
Leo Basov <l.basov@sparc-industries.com>
2022-08-02 08:35:43 +0200

    added glider test

--- test/unit_tests/al/lenia.h
+++ test/unit_tests/al/lenia.h
@@ -88,4 +88,44 @@ TEST(Lenia, GameOfLife_Blinker) {
     ASSERT_DOUBLE_EQ(1.0, lenia.value(3, 2));
 }
 
+TEST(Lenia, GameOfLife_Glider) {
+    Lenia lenia;
+    Config config;
+
+    config.x = 6;
+    config.y = 6;
+    config.type = kernel::Type::GAME_OF_LIFE;
+    config.dt = 1.0;
+    config.mu = 3.0;
+    config.sigma = 0.5;
+
+    lenia.SetConfig(config);
+
+    lenia.value(1, 3) = 1.0;
+    lenia.value(2, 2) = 1.0;
+    lenia.value(3, 2) = 1.0;
+    lenia.value(3, 3) = 1.0;
+    lenia.value(3, 4) = 1.0;
+
+    lenia.Process();
+
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(2, 2));
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(2, 4));
+
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(3, 2));
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(3, 3));
+
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(4, 3));
+
+    lenia.Process();
+
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(2, 2));
+
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(3, 2));
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(3, 4));
+
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(4, 2));
+    ASSERT_DOUBLE_EQ(1.0, lenia.value(4, 3));
+}
+
 }  // namespace ca
