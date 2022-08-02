f199816df026ba5ffc7e4bbfdbb3ce5735a87931
Leo Basov <l.basov@sparc-industries.com>
2022-08-02 08:12:40 +0200

    extended unit tests for game of life lenia

--- test/unit_tests/al/lenia.h
+++ test/unit_tests/al/lenia.h
@@ -12,7 +12,7 @@ TEST(Lenia, GameOfLife_Block) {
 
     config.x = 4;
     config.y = 4;
-    config.type = kernel::Type::MOORE;
+    config.type = kernel::Type::GAME_OF_LIFE;
     config.dt = 1.0;
     config.mu = 4;
     config.sigma = 0.1;
@@ -23,6 +23,37 @@ TEST(Lenia, GameOfLife_Block) {
     lenia.value(2, 1) = 1.0;
     lenia.value(1, 2) = 1.0;
     lenia.value(2, 2) = 1.0;
+
+    lenia.Process();
+
+    for (size_t x = 0; x < lenia.size_x(); x++) {
+        for (size_t y = 0; y < lenia.size_y(); y++) {
+            ASSERT_DOUBLE_EQ(lenia.value(x, y), lenia.value(x, y));
+        }
+    }
+}
+
+TEST(Lenia, GameOfLife_BeeHive) {
+    Lenia lenia;
+    Config config;
+
+    config.x = 4;
+    config.y = 4;
+    config.type = kernel::Type::GAME_OF_LIFE;
+    config.dt = 1.0;
+    config.mu = 4;
+    config.sigma = 0.1;
+
+    lenia.SetConfig(config);
+
+    lenia.value(1, 2) = 1.0;
+    lenia.value(4, 2) = 1.0;
+
+    lenia.value(2, 1) = 1.0;
+    lenia.value(3, 1) = 1.0;
+
+    lenia.value(2, 3) = 1.0;
+    lenia.value(3, 3) = 1.0;
 
     lenia.Process();
 
