e4c166cefdb68ee9c89676976919a13767e3a944
Leo Basov <l.basov@sparc-industries.com>
2022-08-03 17:30:19 +0200

    adde GetNorm function to new Kernel

--- src/kernel/kernel.cc
+++ src/kernel/kernel.cc
@@ -4,4 +4,6 @@ namespace ca {
 
 Kernel::Kernel() {}
 
+double Kernel::GetNorm() const { return norm_; }
+
 }  // namespace ca
--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -10,6 +10,7 @@ class Kernel {
     ~Kernel() = default;
 
     virtual double Value(const double& x_diff, const double& y_diff) const = 0;
+    double GetNorm() const;
 
    protected:
     double norm_ = 1.0;
