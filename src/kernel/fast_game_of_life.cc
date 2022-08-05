4c16edf716c86c083794049ee4a5d43ee5a16bc9
Leo Basov <l.basov@sparc-industries.com>
2022-08-05 10:36:55 +0200

    added factor

--- src/kernel/fast_game_of_life.cc
+++ src/kernel/fast_game_of_life.cc
@@ -6,6 +6,7 @@ FastGameOfLife::FastGameOfLife() {}
 
 void FastGameOfLife::SetUp(const FastGrid &grid) {
     const int size = grid.x_ * grid.y_;
+    const double factor = 1.0 / 8.5;
     kernel_ = MatrixXd::Zero(size, size);
 
     for (int x = 0; x < grid.x_; x++) {
@@ -16,11 +17,11 @@ void FastGameOfLife::SetUp(const FastGrid &grid) {
             const int idx_ym = grid.idx(x, y - 1);
             const int idx_yp = grid.idx(x, y + 1);
 
-            kernel_(idx, idx) = 0.5;
-            kernel_(idx_xm, idx) = 1.0;
-            kernel_(idx_xp, idx) = 1.0;
-            kernel_(idx_ym, idx) = 1.0;
-            kernel_(idx_yp, idx) = 1.0;
+            kernel_(idx, idx) = 0.5 * factor;
+            kernel_(idx_xm, idx) = factor;
+            kernel_(idx_xp, idx) = factor;
+            kernel_(idx_ym, idx) = factor;
+            kernel_(idx_yp, idx) = factor;
         }
     }
 }
